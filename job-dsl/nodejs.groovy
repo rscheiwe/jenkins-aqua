77a5fd1442d552df2fc75d346242c870cb98ec9a
rscheiwe <rscheiwe@gmail.com>
2019-06-30 12:50:43 -0400

    specified groovyjs paths

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,15 +1,15 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/rscheiwe/docker-aqua.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('rscheiwe')
+            node / gitConfigEmail('rscheiwe@gmail.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('NodeJS') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
